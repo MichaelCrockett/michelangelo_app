299fdbd1cf0c86e6a3d0cb8aa66aa0027101e676
Michael Crockett <mike.crockett76@gmail.com>
2021-02-15 15:41:08 +0000

    Added and styled a header component to be used on all pages

---
+++ components/Header.vue
@@ -0,0 +1,42 @@
+<template lang="html">
+  <header>
+    <nav>
+      <ul>
+        <li><nuxt-link to="/">Start page</nuxt-link></li>
+        <li><nuxt-link to="/works">Works of art</nuxt-link></li>
+        <li><nuxt-link to="/about">About</nuxt-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+
+<style scoped>
+
+header {
+  width: 100%;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: wheat;
+}
+
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+li {
+  margin: 0 10px;
+}
+a {
+  text-decoration: none;
+  color: black;
+}
+
+</style>
